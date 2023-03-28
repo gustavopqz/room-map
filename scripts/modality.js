93228775749ee9cc16d38f8e7b8448aec3bbfdb2
Gustavo Pasqua de Queiroz <dev.gustavo@outlook.com>
2023-03-28 09:05:23 -0300

    Mapa de salas - Front-end

---
+++ scripts/modality.js
@@ -0,0 +1,10 @@
+//Event listener in modality
+const modality = document.getElementById('modality')
+
+modality.addEventListener('change', ()=>{
+    if(modality.value == 'pós'){
+        if(window.innerWidth > 680){
+            
+        }
+    }
+})
